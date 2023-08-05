30f9db9621c725c127d4ae6e386b020b570b2ee0
Muhammad Urwah <urwahpatel@gmail.com>
2023-08-05 16:14:02 +0500

    Updated Policy

--- frontend/pages/returnpolicy.tsx
+++ frontend/pages/returnpolicy.tsx
@@ -1,5 +1,5 @@
 import { useTitle } from "@/context/titleContext";
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
 export default function ReturnPolicy() {
   const { changeTitle } = useTitle();
@@ -9,9 +9,32 @@ export default function ReturnPolicy() {
   }, []);
 
   return (
-    <div className='w-full'>
-      <h1 className='page-header'>Return Policy</h1>
-      <h3 className="text-center">Policy is being updated Currently.</h3>
+    <div className="md:max-w-5xl w-11/12 mx-auto">
+      <h1 className="page-header">Return Policy</h1>
+      <div className="flex flex-col gap-4 text-sm text-zinc-500">
+        <p>
+          All items are carefully checked before dispatch to ensure that your
+          purchase is in excellent condition. If you are still not satisfied or
+          an item delivered is found broken, then you can exchange or return it
+          within seven days of purchase, and you will receive a 100% FULL REFUND
+          ( Not applicable on Perfume Collection ).
+        </p>
+        <p className="font-bold">
+          However, for a product to be successfully refunded or exchanged :
+        </p>
+        <ul className="policy">
+          <li>The item must be unused. </li>
+          <li>It should be in its original packaging. </li>
+          <li>The receipt should also be on display. </li>
+        </ul>
+        <p className="font-bold">Our Refund and Exchange Procedure:</p>
+        
+        <ul className="policy">
+          <li>The item must be unused. </li>
+          <li>It should be in its original packaging. </li>
+          <li>The receipt should also be on display. </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
